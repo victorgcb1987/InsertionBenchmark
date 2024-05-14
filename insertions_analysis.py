ac70a30021efa617cfd8ebc92b2e52953c810392
Victor García-Carpintero Burgos <victor.gcb@gmail.com>
2024-05-14 15:18:53 +0200

    Update insertions_analysis.py

--- insertions_analysis.py
+++ insertions_analysis.py
@@ -83,19 +83,15 @@ def main():
         insertions_df = load_insertions_source_as_dataframe(summary, repetitive_regions=test_ir)
         insertions_df.to_csv("check.tsv", sep="\t")
         minimap2_df = load_minimap2_hits_as_dataframe(mapping_output, repetitive_regions=test_ir)
-        #print(minimap2_df)
         minimap2_df.to_csv("minimap2.tsv", sep="\t", index=False)
         sequences_in_nucleus_df = load_read_positions_from_maf_into_dataframe(in_fpath / "{}_0001.maf.gz".format(arguments["prefix"]))
         total_sequences = set(sequences_in_nucleus_df['readName'].tolist())
         reads_from_insertions_df = get_reads_from_insertions(insertions_df, sequences_in_nucleus_df)
-        #print(reads_from_insertions_df)
         from_insertions_readnames = set(reads_from_insertions_df['readName'].tolist())
-        #print(len(from_insertions_readnames))
-        #reads_not_from_insertions_df = filter_by_readname(reads_from_insertions_df, from_insertions_readnames, mode="exclude")
         not_from_insertions_readnames = total_sequences - from_insertions_readnames
         mapped_reads = set(minimap2_df['readName'].tolist())
-        mapped_reads_from_insertions = mapped_reads -  not_from_insertions_readnames
-        mapped_reads_not_from_insertions = mapped_reads - from_insertions_readnames
+        mapped_reads_from_insertions = mapped_reads.intersection(from_insertions_readnames)
+        mapped_reads_not_from_insertions = mapped_reads.intersection(not_from_insertions_readnames)
         results_dict["identity"].append(identity)
         results_dict["total_reads"].append(len(total_sequences))
         results_dict["reads_from_insertions"].append(len(from_insertions_readnames))
